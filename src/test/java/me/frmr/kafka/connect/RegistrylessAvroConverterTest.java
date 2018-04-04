fa371ab95068a70676b9182e9c004fb8c6c2faeb
Matt Farmer <matt@frmr.me>
2018-04-04 19:13:39 -0400

    Add a WIP test on fromConnectData

--- src/test/java/me/frmr/kafka/connect/RegistrylessAvroConverterTest.java
+++ src/test/java/me/frmr/kafka/connect/RegistrylessAvroConverterTest.java
@@ -18,12 +18,19 @@ package me.frmr.kafka.connect;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.SeekableByteArrayInput;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.kafka.connect.data.*;
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.InputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -69,8 +76,44 @@ class RegistrylessAvroConverterTest {
 
 
   @Test
-  void fromConnectDataWorks() {
-    // todo
+  void fromConnectDataWorks() throws Exception {
+    // This only has to work in the project directory because this is a test. I'm not particularly
+    // concerned if it works when the tests are packaged in JAR form right now. If we start doing
+    // that then we'll do something clever-er.
+    String validSchemaPath = new File("src/test/resources/schema/dog.avsc").getAbsolutePath();
+
+    RegistrylessAvroConverter sut = new RegistrylessAvroConverter();
+    Map<String, Object> settings = new HashMap<String, Object>();
+    settings.put("schema.path", validSchemaPath);
+    sut.configure(settings, false);
+
+    Schema dogSchema = SchemaBuilder.struct()
+      .name("dog")
+      .field("name", Schema.STRING_SCHEMA)
+      .field("breed", Schema.STRING_SCHEMA)
+      .build();
+
+    Struct dogStruct = new Struct(dogSchema)
+      .put("name", "Beamer")
+      .put("breed", "Boarder Collie");
+
+    byte[] result = sut.fromConnectData("test_topic", dogSchema, dogStruct);
+    FileUtils.writeByteArrayToFile(new File("example.avro"), result);
+
+    // This is a bit annoying but because of the way avro works - the resulting byte array isn't
+    // deterministic - so we need to read it back using the avro tools.
+    DatumReader<GenericRecord> datumReader = new GenericDatumReader<>();
+    GenericRecord instance = null;
+    try (
+      SeekableByteArrayInput sbai = new SeekableByteArrayInput(result);
+      DataFileReader<GenericRecord> dataFileReader = new DataFileReader<>(sbai, datumReader);
+    ) {
+      assertEquals(1, dataFileReader.getBlockCount());
+      instance = dataFileReader.next();
+    } catch (IOException ioe) {
+      throw new Exception("Failed to deserialize Avro data", ioe);
+    }
+
   }
 
   @Test
@@ -86,7 +129,6 @@ class RegistrylessAvroConverterTest {
     RegistrylessAvroConverter sut = new RegistrylessAvroConverter();
     Map<String, Object> settings = new HashMap<String, Object>();
     settings.put("schema.path", validSchemaPath);
-
     sut.configure(settings, false);
 
     SchemaAndValue sav = sut.toConnectData("test_topic", dogData);
